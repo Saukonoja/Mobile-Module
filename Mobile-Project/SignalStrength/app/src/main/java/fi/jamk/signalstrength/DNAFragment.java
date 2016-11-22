819c105bce9b1ae7d00f857eb13d29289fea8f90
Janne Hakala <janne.hakala94@hotmail.fi>
2016-11-22 21:42:32 +0200

    Add map to fragment

---
+++ Mobile-Project/SignalStrength/app/src/main/java/fi/jamk/signalstrength/DNAFragment.java
@@ -0,0 +1,18 @@
+package fi.jamk.signalstrength;
+
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DNAFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View rootView = inflater.inflate(R.layout.fragment_dna, container, false);
+        //TextView textView = (TextView) rootView.findViewById(R.id.dna);
+        //textView.setPaintFlags(textView.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        return rootView;
+    }
+}
---
+++ Mobile-Project/SignalStrength/app/src/main/java/fi/jamk/signalstrength/SoneraFragment.java
@@ -0,0 +1,111 @@
+package fi.jamk.signalstrength;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class SoneraFragment extends Fragment implements OnMapReadyCallback {
+    MapView mMapView;
+    private GoogleMap mMap;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_sonera, container, false);
+
+        mMapView = (MapView) rootView.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume(); // needed to get the map to display immediately
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        FetchDataTask task = new FetchDataTask();
+        task.execute("http://ptm.fi/jamk/android/golf_courses.json");
+        return rootView;
+    }
+    @Override
+    public void onMapReady (GoogleMap googleMap){
+
+        // store map object to member variable
+        mMap = googleMap;
+        // set map type
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+
+    }
+
+    class FetchDataTask extends AsyncTask<String, Void, JSONObject> {
+        @Override
+        protected JSONObject doInBackground(String... urls) {
+            HttpURLConnection urlConnection = null;
+            JSONObject json = null;
+            try {
+                URL url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line).append("\n");
+                }
+                bufferedReader.close();
+                json = new JSONObject(stringBuilder.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } finally {
+                if (urlConnection != null) urlConnection.disconnect();
+            }
+
+            return json;
+        }
+    }
+        protected void onPostExecute(JSONObject json) {
+
+
+
+
+
+
+
+
+
+
+    }
+}
