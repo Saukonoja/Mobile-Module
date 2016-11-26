139a312aff39f82d5162911663811daba05a1cc8
Janne Hakala <janne.hakala94@hotmail.fi>
2016-11-26 18:57:58 +0200

    Finish menus

--- Mobile-Project/SignalStrength/app/src/main/java/fi/jamk/signalstrength/MainActivity.java
+++ Mobile-Project/SignalStrength/app/src/main/java/fi/jamk/signalstrength/MainActivity.java
@@ -1,33 +1,71 @@
 package fi.jamk.signalstrength;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Paint;
-import android.graphics.Typeface;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.telephony.PhoneStateListener;
+import android.telephony.SignalStrength;
+import android.telephony.TelephonyManager;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.Toast;
+import android.Manifest;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 
-import android.widget.TextView;
+import java.util.List;
+import java.util.Locale;
 
-import org.w3c.dom.Text;
+public class MainActivity extends AppCompatActivity implements
+        ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
 
-public class MainActivity extends AppCompatActivity {
+    private TelephonyManager mTelephonyManager;
+    private MyPhoneStateListener mPhoneStatelistener;
 
     private SectionsPagerAdapter mSectionsPagerAdapter;
-
     private ViewPager mViewPager;
+
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+
+    private final int REQUEST_LOCATION = 1;
+
+    private double lat = 0;
+    private double lon = 0;
+
+    protected int mGsmSignalStrength = 0;
+    protected int mCdmaSignalStrength = 0;
+    protected int mEvdoSignalStrength = 0;
+
+    private String operatorName;
+
+    private boolean IsTracking;
+    private Switch switchTracking;
+    private Menu menu;
+    private MenuItem menuTracking;
 
 
     @Override
@@ -35,6 +73,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.menu = menu;
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         // Create the adapter that will return a fragment for each of the three
@@ -45,23 +84,128 @@ public class MainActivity extends AppCompatActivity {
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mPhoneStatelistener = new MyPhoneStateListener();
+        mTelephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
 
+        operatorName = mTelephonyManager.getNetworkOperatorName();
+
+        switchTracking = new Switch(MainActivity.this);
+        switchTracking.setChecked(true);
+
+        IsTracking = true;
     }
 
+    @Override
+    public void onConnected(Bundle bundle) {
+        // Connected to Google Play services.
+        checkPermissions();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        // The connection has been interrupted.
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        //This callback is important for handling errors.
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (IsTracking) {
+            lat = location.getLatitude();
+            lon = location.getLongitude();
+
+            /* ei toiminut
+            try {
+                Geocoder gcd = new Geocoder(this, Locale.getDefault());
+                List<Address> addresses = gcd.getFromLocation(62.2333333, 25.7333333, 1);
+                if (addresses.size() > 0) {
+                    showToast(addresses.get(0).getLocality());
+                } else {
+
+                }
+            }catch (Exception ex){
+
+            }*/
+
+            //kaa.SendLog(operatorName, lat, lon, mGsmSignalStrength, mCdmaSignalStrength, mEvdoSignalStrength);
+        }
+    }
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    private void checkPermissions() {
+        // check permission
+        int hasLocationPermission = ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION);
+        // permission is not granted yet
+        if (hasLocationPermission != PackageManager.PERMISSION_GRANTED) {
+            // ask it -> a dialog will be opened
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        } else {
+            // permission is already granted, start get location information
+            startGettingLocation();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_LOCATION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // accepted, start getting location information
+                    startGettingLocation();
+                } else {
+                    // denied
+                    Toast.makeText(this, "Location access denied by the user!", Toast.LENGTH_LONG).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    private void startGettingLocation() {
+            mLocationRequest = LocationRequest.create();
+            mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            mLocationRequest.setInterval(1000); // Update location every second
+
+            // now permission is granted, but we need to check it
+            int hasLocationPermission = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+            if (hasLocationPermission == PackageManager.PERMISSION_GRANTED) {
+                // start requesting location changes
+                LocationServices.FusedLocationApi.requestLocationUpdates(
+                        mGoogleApiClient, mLocationRequest, this);
+            }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        menuTracking = menu.findItem(R.id.enable_tracking);
         return true;
     }
 
@@ -79,8 +223,47 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         if (id == R.id.enable_tracking) {
-            TrackingDialogFragment eDialog = new TrackingDialogFragment();
-            eDialog.show(getFragmentManager(), "tracking");
+
+            switchTracking.setTextOn("päälle");
+            switchTracking.setTextOff("pois");
+
+            final LinearLayout linearLayout = new LinearLayout(MainActivity.this);
+            linearLayout.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT));
+            linearLayout.setGravity(Gravity.CENTER_HORIZONTAL);
+            linearLayout.addView(switchTracking);
+
+            AlertDialog.Builder myDialog = new AlertDialog.Builder(MainActivity.this);
+            myDialog.setTitle("Paikannus")
+            .setMessage("Aseta paikannus päälle tai pois.")
+            .setView(linearLayout)
+            .setOnCancelListener(
+                    new DialogInterface.OnCancelListener() {
+                        @Override
+                        public void onCancel(DialogInterface dialog) {
+                            linearLayout.removeAllViews();
+                        }
+                    })
+            .setPositiveButton(R.string.dialog_ok, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    linearLayout.removeAllViews();
+                }
+            });
+
+            myDialog.show();
+
+            switchTracking.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (isChecked){
+                        IsTracking = true;
+                        menuTracking.setTitle("Paikannus (päällä)");
+                    }else{
+                        IsTracking = false;
+                        menuTracking.setTitle("Paikannus (pois)");
+                    }
+                }
+            });
+
             return true;
         }
 
@@ -102,13 +285,10 @@ public class MainActivity extends AppCompatActivity {
         public Fragment getItem(int position) {
             switch (position) {
                 case 0:
-
                     return new SoneraFragment();
                 case 1:
-                    //textViewSonera.setPaintFlags(textViewSonera.getPaintFlags() & (~ Paint.STRIKE_THRU_TEXT_FLAG));
                     return new DNAFragment();
                 case 2:
-
                     return new SaunalahtiFragment();
             }
             return null;
@@ -119,33 +299,20 @@ public class MainActivity extends AppCompatActivity {
             // Show 3 total pages.
             return 3;
         }
+    }
+
+    class MyPhoneStateListener extends PhoneStateListener {
 
         @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "SONERA";
-                case 1:
-                    return "DNA";
-                case 2:
-                    return "SAUNALAHTI";
-            }
-            return null;
+        public void onSignalStrengthsChanged(SignalStrength signalStrength) {
+            super.onSignalStrengthsChanged(signalStrength);
+
+            mGsmSignalStrength = signalStrength.getGsmSignalStrength();
+            mGsmSignalStrength = (2 * mGsmSignalStrength) - 113; // -> dBm
+
+            mCdmaSignalStrength = signalStrength.getCdmaDbm();
+
+            mEvdoSignalStrength = signalStrength.getGsmBitErrorRate();
         }
-    }
-
-    public static class PlaceholderFragment extends Fragment {
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-
     }
 }
-
--- Mobile-Project/SignalStrength/app/src/main/java/fi/jamk/signalstrength/PersonalActivity.java
+++ Mobile-Project/SignalStrength/app/src/main/java/fi/jamk/signalstrength/PersonalActivity.java
@@ -2,60 +2,76 @@ package fi.jamk.signalstrength;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.telephony.PhoneStateListener;
 import android.telephony.SignalStrength;
 import android.telephony.TelephonyManager;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.TextView;
 
 
-public class PersonalActivity extends Activity{
+public class PersonalActivity extends AppCompatActivity {
 
     private TelephonyManager mTelephonyManager;
     private MyPhoneStateListener mPhoneStatelistener;
 
     private String operatorName;
 
-    private double lat = 0;
-    private double lon = 0;
-
     protected int mGsmSignalStrength = 0;
     protected int mCdmaSignalStrength = 0;
     protected int mEvdoSignalStrength = 0;
 
+    private TextView mPersonalInfoTextView;
     private TextView mOperatorNameTextView;
     private TextView mGsmTextView;
     private TextView mCdmaTextView;
     private TextView mEvdoTextView;
+    private TextView mGsmTextViewColor;
+    private TextView mCdmaTextViewColor;
+    private TextView mEvdoTextViewColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal);
 
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
 
-        TextView text = new TextView(this);
-        text.setText("Signaalin vahvuus");
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        text.setTextAppearance(this, android.R.style.TextAppearance_Material_Widget_ActionBar_Title_Inverse);
-        toolbar.addView(text);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        myToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         // get TextViews
+        mPersonalInfoTextView = (TextView) findViewById(R.id.personalInfoTextView);
         mOperatorNameTextView = (TextView) findViewById((R.id.operatorNameTextView));
         mGsmTextView = (TextView)findViewById(R.id.gsmTextView);
         mCdmaTextView = (TextView)findViewById(R.id.cdmaTextView);
         mEvdoTextView = (TextView)findViewById(R.id.evdoTextView);
+        mGsmTextViewColor = (TextView) findViewById(R.id.gsmTextViewColor);
+        mCdmaTextViewColor = (TextView) findViewById(R.id.cdmaTextViewColor);
+        mEvdoTextViewColor = (TextView) findViewById(R.id.evdoTextViewColor);
 
         mPhoneStatelistener = new MyPhoneStateListener();
         mTelephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
 
         operatorName = mTelephonyManager.getNetworkOperatorName();
-        mOperatorNameTextView.setText("Operaattori: "+operatorName);
+        mOperatorNameTextView.setText("Operaattori: "+ operatorName);
 
         mTelephonyManager.listen(mPhoneStatelistener,PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
 
+        mPersonalInfoTextView.setPaintFlags(mPersonalInfoTextView.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
     }
 
     class MyPhoneStateListener extends PhoneStateListener {
@@ -71,9 +87,34 @@ public class PersonalActivity extends Activity{
 
             mEvdoSignalStrength = signalStrength.getGsmBitErrorRate();
 
-            mGsmTextView.setText(Integer.toString(mGsmSignalStrength));
-            mCdmaTextView.setText(Integer.toString(mCdmaSignalStrength));
-            mEvdoTextView.setText(Integer.toString(mEvdoSignalStrength));
+            mGsmTextView.setText("GSM " + Integer.toString(mGsmSignalStrength) + " dBm");
+            mCdmaTextView.setText("CDMA " + Integer.toString(mCdmaSignalStrength) + " dBm");
+            mEvdoTextView.setText("EVDO " + Integer.toString(mEvdoSignalStrength) + " dBm");
+
+            if (mGsmSignalStrength > -65 || mCdmaSignalStrength > - 65) {
+                mGsmTextViewColor.setText("PERFECT");mGsmTextViewColor.setBackgroundResource(R.color.green);
+                mCdmaTextViewColor.setText("PERFECT");mCdmaTextViewColor.setBackgroundResource(R.color.green);
+            }
+
+            if (mGsmSignalStrength >= -79 && mGsmSignalStrength <= -65 || mCdmaSignalStrength >= -79 && mCdmaSignalStrength <= -65) {
+                mGsmTextViewColor.setText("GOOD");mGsmTextViewColor.setBackgroundResource(R.color.yellow);
+                mCdmaTextViewColor.setText("GOOD");mCdmaTextViewColor.setBackgroundResource(R.color.yellow);
+            }
+
+            if (mGsmSignalStrength >= -89 && mGsmSignalStrength <= -80 || mCdmaSignalStrength >= -89 && mCdmaSignalStrength <= -80) {
+                mGsmTextViewColor.setText("FAIR");mGsmTextViewColor.setBackgroundResource(R.color.orange);
+                mCdmaTextViewColor.setText("FAIR");mCdmaTextViewColor.setBackgroundResource(R.color.orange);
+            }
+
+            if (mGsmSignalStrength >= -99 && mGsmSignalStrength <= -90 || mCdmaSignalStrength >= -99 && mCdmaSignalStrength <= -90) {
+                mGsmTextViewColor.setText("POOR");mGsmTextViewColor.setBackgroundResource(R.color.magenta);
+                mCdmaTextViewColor.setText("POOR");mCdmaTextViewColor.setBackgroundResource(R.color.magenta);
+            }
+
+            if (mGsmSignalStrength <= -100 || mCdmaSignalStrength <= -100) {
+                mGsmTextViewColor.setText("NO SIGNAL");mGsmTextViewColor.setBackgroundResource(R.color.red);
+                mCdmaTextViewColor.setText("NO SIGNAL");mCdmaTextViewColor.setBackgroundResource(R.color.red);
+            }
         }
     }
 }
